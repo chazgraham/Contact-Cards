ad2a70269cd7e79fa887853d51a3bbaef3cc3fa7
chaz Graham <chazhg80.cg@gmail.com>
2022-11-22 12:59:05 -0600

    added edit card

--- client/src/js/index.js
+++ client/src/js/index.js
@@ -17,6 +17,7 @@ import { getDb } from './database';
 import { postDb } from './database';
 import { fetchCards } from './cards';
 import { deleteDb } from './database';
+import { editDb } from './database';
 
 window.addEventListener('load', function () {
     initdb();
@@ -48,7 +49,13 @@ form.addEventListener('submit', event => {
     if (submitBtnToUpdate == false) {
         postDb(name, email, phone, profile);
     } else {
-
+        // Obtains values passed into the form element
+        let name = document.getElementById("name").value;
+        let phone = document.getElementById("phone").value;
+        let email = document.getElementById("email").value;
+        let profile = document.querySelector('input[type="radio"]:checked').value;
+        // Calls the editDB function passing in any values from the form element as well as the ID of the contact that we are updating
+        editDb(profileId, name, email, phone, profile);
         fetchCards();
         // Toggles the submit button back to POST functionality
         submitBtnToUpdate = false;
@@ -69,4 +76,23 @@ window.deleteCard = (e) => {
     deleteDb(id);
     // Reload the DOM
     fetchCards();
+};
+
+window.editCard = (e) => {
+    // Grabs the id from the button element attached to the contact card and sets a global variable that will be used in the form element.
+    profileId = parseInt(e.dataset.id);
+  
+    // Grabs information to pre-populate edit form
+    let editName = e.dataset.name;
+    let editEmail = e.dataset.email;
+    let editPhone = e.dataset.phone;
+  
+    document.getElementById("name").value = editName;
+    document.getElementById("email").value = editEmail;
+    document.getElementById("phone").value = editPhone;
+  
+    form.style.display = "block";
+  
+    // Toggles the Submit button so that it now Updates an existing contact instead of posting a new one
+      submitBtnToUpdate = true;
 };
